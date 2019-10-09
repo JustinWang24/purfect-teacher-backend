0e05deb54a0cbee30ee165626d14d5b4ad2e9114
wangyue <justinwang24@yahoo.com.au>
2019-10-09 13:53:20 +0800

    Update template

--- resources/views/layouts/desktop/footer.blade.php
+++ resources/views/layouts/desktop/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="page-footer">
-    <div class="page-footer-inner"> {{ date('Y') }} &copy; 帕菲特
+    <div class="page-footer-inner"> {{ date('Y') }} &copy; {{ env('APP_NAME') }}
         <a href="mailto:wangyue@purfect.cn" target="_top" class="makerCss">技术支持</a>
     </div>
     <div class="scroll-to-top">
