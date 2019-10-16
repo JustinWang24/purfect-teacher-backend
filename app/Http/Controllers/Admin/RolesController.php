e1b08d1ddec86bdc4c7d4c9fe04e20f9d57c69e5
wangyue <justinwang24@yahoo.com.au>
2019-10-16 20:09:42 +0800

    New feat: 自动面包屑模块

--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -6,11 +6,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\RoleRequest;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class RolesController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function index(RoleRequest $request){
-        dd(get_class($request));
+        $this->dataForView['pageTitle'] = trans('thumbnail.roles');
+        return view('admin.roles.list', $this->dataForView);
     }
 }
