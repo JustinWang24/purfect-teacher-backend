8d7cd9aa42d813cc9ce304b91ec1be4568276b11
wangyue <justinwang24@yahoo.com.au>
2019-10-16 22:24:32 +0800

    Keep add more basic functions

--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -4,9 +4,10 @@
  */
 namespace App\Http\Controllers\Admin;
 
+use App\Dao\Users\RoleDao;
 use App\Http\Requests\RoleRequest;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Acl\Role;
 
 class RolesController extends Controller
 {
@@ -15,8 +16,25 @@ class RolesController extends Controller
         $this->middleware('auth');
     }
 
+    /**
+     * @param RoleRequest $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index(RoleRequest $request){
         $this->dataForView['pageTitle'] = trans('thumbnail.roles');
+        $this->dataForView['roles'] = Role::AllNames();
         return view('admin.roles.list', $this->dataForView);
+    }
+
+    /**
+     * @param RoleRequest $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(RoleRequest $request){
+        $dao = new RoleDao();
+        $role = $dao->getBySlug($request->get('slug'));
+        $this->dataForView['roles'] = $role;
+        $this->dataForView['pageTitle'] = '编辑角色权限: '.$role->name;
+        return view('admin.roles.edit', $this->dataForView);
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,5 @@ Route::prefix('admin')->group(function () {
     Route::get('index', 'IndexController@index');
     // 角色与权限管理路由组
     Route::get('roles/list', 'RolesController@index')->name('admin.roles.list');
+    Route::get('roles/edit', 'RolesController@edit')->name('admin.roles.edit');
 });
