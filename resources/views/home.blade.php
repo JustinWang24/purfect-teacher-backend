c47279bd16b3ba7bc6f152e143cd6b63d9a12187
wangyue <justinwang24@yahoo.com.au>
2019-10-16 20:09:42 +0800

    New feat: 自动面包屑模块

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<div class="page-content">
-    <div class="page-bar">
-        <div class="page-title-breadcrumb">
-            <div class=" pull-left">
-                <div class="page-title">Dashboard</div>
-            </div>
-            <ol class="breadcrumb page-breadcrumb pull-right">
-                <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item"
-                                                       href="index.html">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
-                </li>
-                <li class="active">Dashboard</li>
-            </ol>
-        </div>
-    </div>
     <!-- start widget -->
     <div class="state-overview">
         <div class="row">
@@ -724,5 +710,4 @@
         </div>
     </div>
     <!-- end new student list -->
-</div>
 @endsection
--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,7 @@
 <?php
-
 // 管理后台路由
 Route::prefix('admin')->group(function () {
     Route::get('index', 'IndexController@index');
-
     // 角色与权限管理路由组
-    Route::get('roles', 'RolesController@index')->name('admin.roles.list');
+    Route::get('roles/list', 'RolesController@index')->name('admin.roles.list');
 });
