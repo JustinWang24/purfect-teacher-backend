444c441689a6876e9c16a232363fd9c1a80180ff
ma <admin@admin.com>
2019-10-19 11:00:23 +0800

    school字段可以为空

--- database/migrations/2019_10_18_233121_create_schools_table.php
+++ database/migrations/2019_10_18_233121_create_schools_table.php
@@ -22,9 +22,9 @@ class CreateSchoolsTable extends Migration
                 $table->unsignedMediumInteger('max_students_number')->default(0)->comment('该学校所支持的最大学生账户数, 超过次数量则不予开放新学生账号注册功能. 0 表示无限制');
                 $table->unsignedMediumInteger('max_employees_number')->default(0)->comment('该学校所支持的最大教职工账户数, 超过次数量则不予开放教职工账号注册功能. 0 表示无限制');
                 $table->string('name', 100)->comment('学校名称');
-                $table->string('logo', 200)->comment('学校logo');
-                $table->string('video', 200)->comment('学校视频');
-                $table->string('motto', 100)->comment('校训');
+                $table->string('logo', 200)->comment('学校logo')->nullable();;
+                $table->string('video', 200)->comment('学校视频')->nullable();;
+                $table->string('motto', 100)->comment('校训')->nullable();;
                 $table->unsignedBigInteger('last_updated_by')->default(0)->comment('最后更新该学校的信息的用户');
                 $table->timestamps();
                 $table->softDeletes();
