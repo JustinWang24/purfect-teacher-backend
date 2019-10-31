ec6a9dddc7b51601584361261b8b5c8b5c22ad20
vagrant <vagrant@homestead>
2019-10-31 12:43:25 +0000

    报名管理

---
+++ app/Http/Controllers/Operator/RecruitStudent/RegistrationInformation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Operator\RecruitStudent;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RecruitStudent\RegistrationInformationRequest;
+
+class RegistrationInformation extends Controller
+{
+
+    /**
+     * 报名管理
+     * @param RegistrationInformationRequest $request
+     */
+    public function index(RegistrationInformationRequest $request)
+    {
+
+    }
+
+}
