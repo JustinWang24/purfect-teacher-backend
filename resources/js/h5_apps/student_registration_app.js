4dbe59bf70d424f394edd36bcbf4a19db82f25d8
wangyue <justinwang24@yahoo.com.au>
2019-11-01 16:26:45 +0800

    u

---
+++ resources/js/h5_apps/student_registration_app.js
@@ -0,0 +1,7 @@
+window.axios = require('axios');
+window.Vue = require('vue');
+// 引入 Element UI 库
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
