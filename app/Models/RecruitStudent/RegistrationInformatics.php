6036cd146715a606dd33458abbfe4f195c2784e0
vagrant <vagrant@homestead>
2019-11-01 10:56:07 +0000

    新增报名学生seeds

--- app/Models/RecruitStudent/RegistrationInformatics.php
+++ app/Models/RecruitStudent/RegistrationInformatics.php
@@ -7,6 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegistrationInformatics extends Model
 {
-
-
+    protected $fillable = ['user_id', 'school_id', 'major_id', 'name', 'whether_adjust', 'status'];
 }
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,9 +1,16 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Students\StudentProfile;
 use App\User;
+use App\Models\RecruitStudent\RegistrationInformatics;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use App\Models\Acl\Role;
+use Illuminate\Support\Facades\Hash;
+use Ramsey\Uuid\Uuid;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +25,43 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
+        'mobile'=>'3333'.rand(1, 99).rand(1, 99),
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'uuid'=>Uuid::uuid4()->toString(),
+        'password'=>Hash::make('ac59075b964b0715'),
+        'status'=>Role::VISITOR,
+        'type'=>Role::VISITOR,
+        'mobile_verified_at'=>Carbon::now(),
+    ];
+});
+
+$factory->define(StudentProfile::class, function (Faker $faker) {
+    return [
+        'serial_number' => '0',
+        'avatar' => 'www.xx.test',
+        'device' => 'ios',
+        'year' => '2019',
+        'gender' => rand(1, 2),
+        'country' => '北京',
+        'state' => '北京',
+        'city' => '北京市',
+        'area' => '朝阳区',
+        'address_line' => '电信工程局9楼',
+        'id_number' => '2019' . rand(100, 300) . rand(1000, 3000) . rand(3999, 9000),
+        'birthday' => '2019-11-1',
+        'political_name' => '党员',
+        'nation_name' => '叶赫那拉氏',
+        'source_place' => '北京',
+        'parent_name' => '帕菲特',
+        'parent_mobile' => '9999' . rand(1, 99) . rand(1, 99)
+    ];
+});
+
+$factory->define(RegistrationInformatics::class, function (Faker $faker) {
+    return [
+        'school_id' => rand(1, 9),
+        'major_id' => rand(1, 3),
+        'relocation_allowed' => rand(1, 2),
+        'status' => rand(1, 3),
     ];
 });
