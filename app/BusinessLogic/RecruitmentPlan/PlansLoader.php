5394c07ba0894d8e4d9644a706758e4a23eda7e7
wangyue <justinwang24@yahoo.com.au>
2019-11-07 13:35:53 +0800

    修复 Bug , 加载教学计划
    根据是否提交 year 作为区分前后端的条件.
    前端是不提交 year 的, 而是根据当前时间来判定如何加载哪些招生计划

--- app/BusinessLogic/RecruitmentPlan/PlansLoader.php
+++ app/BusinessLogic/RecruitmentPlan/PlansLoader.php
@@ -7,8 +7,6 @@
  */
 
 namespace App\BusinessLogic\RecruitmentPlan;
-
-
 use App\BusinessLogic\RecruitmentPlan\Contract\IPlansLoaderLogic;
 use App\BusinessLogic\RecruitmentPlan\Impl\BackendLogic;
 use App\BusinessLogic\RecruitmentPlan\Impl\FrontendLogic;
@@ -22,13 +20,13 @@ class PlansLoader
      */
     public static function GetInstance(PlanRecruitRequest $request){
         $instance = null;
-        if($request->getVersion()){
-            // 从前端页面传来的
-            $instance = new FrontendLogic($request);
+        if($request->getYear()){
+            // 从管理后台传来的, 会提交 year 作为查询参数, 而前端不会
+            $instance = new BackendLogic($request);
         }
         else{
-            // 从管理后台传来的
-            $instance = new BackendLogic($request);
+            // 从前端页面传来的
+            $instance = new FrontendLogic($request);
         }
         return $instance;
     }
