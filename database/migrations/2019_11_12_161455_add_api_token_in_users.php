e64ccd853d3b138cd41fd196b6359b00e7754079
wangyue <justinwang24@yahoo.com.au>
2019-11-12 18:14:45 +0800

    添加 API 认证的机制

---
+++ database/migrations/2019_11_12_161455_add_api_token_in_users.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
+
+class AddApiTokenInUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('api_token', 80)->after('password')
+                ->unique()
+                ->nullable()
+                ->default(null);
+        });
+        $total = DB::table('users')->count();
+        $id = 1;
+        while ($id <= $total){
+            DB::table('users')->where('id',$id)
+                ->update(['api_token'=>Uuid::uuid4()->toString()]);
+            $id++;
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('api_token');
+        });
+    }
+}
