e69b3410b176bb48823be650dfdf01204602dc7d
vagrant <vagrant@homestead>
2019-11-12 12:49:13 +0000

    通知学生事件

--- app/Events/User/Student/ApproveRegistrationEvent.php
+++ app/Events/User/Student/ApproveRegistrationEvent.php
@@ -2,6 +2,7 @@
 
 namespace App\Events\User\Student;
 
+use App\Dao\Schools\MajorDao;
 use App\Events\CanReachByMobilePhone;
 use App\Models\RecruitStudent\RegistrationInformatics;
 use App\User;
@@ -12,7 +13,13 @@ use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Dao\Schools\SchoolDao;
 
+/**
+ * 当学生的报名表获得批准发送短信
+ * Class ApproveRegistrationEvent
+ * @package App\Events\User\Student
+ */
 class ApproveRegistrationEvent extends AbstractRegistrationEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
@@ -28,13 +35,37 @@ class ApproveRegistrationEvent extends AbstractRegistrationEvent
 
     public function getSmsTemplateId(): string
     {
-        // TODO: 当报名学生的报名表被 pass 后的短信模板 ID 还未知
-        return '';
+        return  '483488';
     }
 
     public function getSmsContent(): array
     {
-        // TODO: 当报名学生的报名表被 pass 后的发送的短信内容
-        return [];
+        return [$this->getUserName(), $this->getSchoolName(), $this->getMajorName(), '通过', $this->getAdmissionOfficeMobile()];
     }
+
+    public function getSchoolName()
+    {
+        $dao = new SchoolDao;
+        $result = $dao->getSchoolById($this->form->school_id);
+        if ($result) {
+            return $result->name;
+        }
+    }
+
+    public function getMajorName()
+    {
+        $dao = new MajorDao;
+        $result = $dao->getMajorById($this->form->major_id);
+        if ($result) {
+            return $result->name;
+        }
+    }
+
+    public function getAdmissionOfficeMobile()
+    {
+        return 100000;
+    }
+
+
+
 }
