58416e678059d96e8eb4ae902ee6623e29966614
wangyue <justinwang24@yahoo.com.au>
2019-11-14 04:16:56 +0800

    创建文件管理器的 UI

---
+++ resources/js/components/fileManager/elements/NewFile.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="new-file-wrapper">
+        <div class="file-icon">
+            <i class="el-icon-document"></i>
+        </div>
+        <div class="file-detail">
+            <p class="file-name">{{ file.name }}</p>
+            <p class="updated-at">{{ file.size }} {{ file.updated_at }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NewFile",
+        props:['file'],
+    }
+</script>
+
+<style scoped lang="scss">
+    $themeColor: rgb(98, 109,183);
+    $colorGrey: rgb(166, 174, 233);
+    .new-file-wrapper{
+        padding: 12px;
+        border-radius: 8px;
+        background-color: white;
+        margin-bottom: 10px;
+        display: flex;
+        justify-content: space-between;
+        .file-icon{
+            color: $themeColor;
+            font-size: 24px;
+            margin: 0 10px 0 10px;
+        }
+        .file-detail{
+            .file-name{
+                font-weight: bold;
+                font-size: 14px;
+                margin:0;
+            }
+            .updated-at{
+                font-size: 12px;
+                color: $colorGrey;
+                margin:0;
+                text-align: right;
+            }
+        }
+    }
+</style>
---
+++ resources/js/components/fileManager/elements/RecentFile.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="recent-file-wrapper" v-on:click="itemClicked">
+        <div class="star-wrap">
+            <i class="el-icon-star-on" v-if="file.star" v-on:click="starClicked"></i>
+            <i class="el-icon-star-off" v-if="!file.star"></i>
+            <more-actions class="txt-white m--10" :file="file" color="rgb(98, 109,183)"></more-actions>
+        </div>
+        <p class="updated-at">{{ file.updated_at }}</p>
+        <p class="file-name">{{ file.name }}</p>
+    </div>
+</template>
+
+<script>
+    import MoreActions from './MoreActions';
+
+    export default {
+        name: "RecentFile",
+        components:{MoreActions},
+        props:['file'],
+        methods: {
+            itemClicked: function(){
+                this.$emit('item-clicked',{file: this.file})
+            },
+            starClicked: function () {
+                this.$emit('star-clicked',{file: this.file})
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    $themeColor: rgb(98, 109,183);
+    $colorWhite: white;
+    $colorGrey: rgb(166, 174, 233);
+    .recent-file-wrapper{
+        background-color: $colorWhite;
+        border-radius: 8px;
+        color: $themeColor !important;
+        width: 130px;
+        height: 160px;
+        margin-bottom: 16px;
+        overflow: hidden;
+        padding: 10px;
+        -webkit-box-shadow: 10px 10px 24px -16px #171517;
+        -moz-box-shadow: 10px 10px 24px -16px #171517;
+        box-shadow: 10px 10px 24px -16px #171517;
+        &:hover{
+            background-color: #e8e8e8;
+        }
+        .star-wrap{
+            display: flex;
+            justify-content: space-between;
+            i{
+                font-size: 24px;
+            }
+            .m--10{
+                margin-top: 0;
+            }
+            .txt-white{
+                color: white;
+                div{
+                    color: white;
+                }
+            }
+        }
+        .updated-at{
+            color: $colorGrey;
+            font-size: 12px;
+            margin-top: 30px;
+        }
+        .file-name{
+            font-size: 15px;
+            line-height: 24px;
+        }
+    }
+</style>
