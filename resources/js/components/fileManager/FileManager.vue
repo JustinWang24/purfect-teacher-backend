bcc5b153af174bbf861179b247cc11fa32bac0fe
wangyue <justinwang24@yahoo.com.au>
2019-11-13 21:33:16 +0800

    u

---
+++ resources/js/components/fileManager/FileManager.vue
@@ -0,0 +1,30 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "FileManager",
+        props:['userUuid'],
+        data() {
+            return {
+                // 当前被选择的目录
+                selectedCategory:{
+                    subs:[],
+                    files:[]
+                },
+                // 当前被选择的文件
+                selectedFile: {
+
+                },
+                // 被加载的所有文件夹
+                categories: [],
+                path:[],
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
