fe82bcfcbdc4471214797346f0f80d5e6bf942f2
zhucg <28435993@qq.com>
2019-11-18 19:43:27 +0800

    选修课申请增加功能

--- app/Models/ElectiveCourses/ApplyCourseArrangement.php
+++ app/Models/ElectiveCourses/ApplyCourseArrangement.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class ApplyCourseArrangement extends Model
 {
     protected $fillable = [
-        'course_id', 'week', 'day_index', 'time_slot_id', 'teacher_notes'
+        'apply_id', 'week', 'day_index', 'time_slot_id',
+        'teacher_notes', 'building_id', 'building_name',
+        'classroom_id', 'classroom_name'
     ];
 }
