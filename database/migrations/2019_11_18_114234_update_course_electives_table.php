a56d93fe365658bcab317df2e24b7d1d3925d920
wangyue <justinwang24@yahoo.com.au>
2019-11-18 15:41:54 +0800

    修复选修课表格失误

--- database/migrations/2019_11_18_114234_update_course_electives_table.php
+++ database/migrations/2019_11_18_114234_update_course_electives_table.php
@@ -14,9 +14,15 @@ class UpdateCourseElectivesTable extends Migration
     public function up()
     {
         Schema::table('course_electives', function (Blueprint $table) {
-            $table->tinyInteger('status')->default(0)->comment('选修课状态:0未开始, 1报名中, 2已截止');
+            $table->unsignedTinyInteger('status')->default(0)->comment('选修课状态:0: 等待, 1: 成功开课, 2: 被管理员取消');
             $table->unsignedSmallInteger('max_num')->default(1)->nullable()->comment('最大人数');
+            $table->timestamp('enrol_start_at')->nullable()->comment('报名开始时间');
             $table->timestamp('expired_at')->nullable()->comment('录取截止时间');
+            $table->text('note')->nullable()->comment('为啥把我取消');
+        });
+
+        Schema::table('teacher_apply_elective_courses', function (Blueprint $table) {
+            $table->renameColumn('min_applied','max_num');
         });
     }
 
@@ -27,6 +33,16 @@ class UpdateCourseElectivesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('course_electives', function (Blueprint $table) {
+            $table->dropColumn('status');
+            $table->dropColumn('max_num');
+            $table->dropColumn('enrol_start_at');
+            $table->dropColumn('expired_at');
+            $table->dropColumn('note');
+        });
+
+        Schema::table('teacher_apply_elective_courses', function (Blueprint $table) {
+            $table->renameColumn('max_num','min_applied');
+        });
     }
 }
