b6927612f9ad806043ce9d76a9f6bab61ce9d201
zhucg <28435993@qq.com>
2019-11-22 10:14:11 +0800

    创建值周功能相关表结构

---
+++ database/migrations/2019_11_22_085459_create_attendance_person.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendancePerson extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance_persons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable(false)->index()->comment('用户id 学生老师都有可能');
+            $table->unsignedTinyInteger('type')->nullable(false)->comment('人员类型，老师还是学生 1 老师 2 学生');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance_persons');
+    }
+}
