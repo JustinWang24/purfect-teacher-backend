d99d53fd37faedfa4091f7bf91fb807007b88dd5
zhucg <28435993@qq.com>
2019-11-22 18:02:04 +0800

    去除校区字段

--- app/Models/AttendanceSchedules/AttendanceSchedule.php
+++ app/Models/AttendanceSchedules/AttendanceSchedule.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class AttendanceSchedule extends Model
 {
     protected $fillable = [
-        'attendance_id', 'user_id', 'start_time', 'end_time', 'time_slot_id', 'school_id', 'campus_id',
+        'attendance_id', 'user_id', 'start_time', 'end_time', 'time_slot_id', 'school_id',
     ];
 }
