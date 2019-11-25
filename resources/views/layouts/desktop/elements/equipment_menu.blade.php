bddc4b4a323efd8e68524cc1cd333e611494e51a
wangyue <justinwang24@yahoo.com.au>
2019-11-26 03:35:03 +0800

    整理一下菜单

--- resources/views/layouts/desktop/elements/equipment_menu.blade.php
+++
@@ -1,6 +0,0 @@
-<li class="nav-item">
-    <a href="{{ route('school_manager.facility.list') }}" class="nav-link">
-        <i class="material-icons">dashboard</i>
-        <span class="title">设备管理</span>
-    </a>
-</li>
--- resources/views/layouts/desktop/elements/home_menu.blade.php
+++ resources/views/layouts/desktop/elements/home_menu.blade.php
@@ -14,5 +14,10 @@
                 <span class="title">组织架构</span>
             </a>
         </li>
+        <li class="nav-item">
+            <a href="{{ route('school_manager.facility.list') }}" class="nav-link">
+                <span class="title">设备管理</span>
+            </a>
+        </li>
     </ul>
 </li>
