202d5c8f5e2e45d92e8346b8dec66ae9f7b92fc4
liuyang <15210631164@163.com>
2019-11-25 09:51:56 +0800

    修改添加班级

--- resources/views/school_manager/grade/edit.blade.php
+++ resources/views/school_manager/grade/edit.blade.php
@@ -1,10 +1,3 @@
-<?php
-use App\Utils\UI\Anchor;
-use App\Utils\UI\Button;
-$currentYear = intval(date('Y'));
-$years = range(2014, $currentYear+1);
-?>
-
 @extends('layouts.app')
 @section('content')
     <div class="row">
@@ -18,28 +11,8 @@ $years = range(2014, $currentYear+1);
                         @csrf
                         <input type="hidden" id="grade-id-input" name="grade[id]" value="{{ $grade->id }}">
                         <input type="hidden" id="grade-major-id-input" name="grade[major_id]" value="{{ $parent->id }}">
-                        <div class="form-group">
-                            <label>入学年份</label>
-                            <select class="form-control" id="grade-year-input" name="grade[year]">
-                                @foreach($years as $year)
-                                    <option value="{{ $year }}" {{ $year===$grade->year ? 'selected' : null }}>{{ $year }}年</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="major-name-input">班级名称</label>
-                            <input required type="text" class="form-control" id="grade-name-input" value="{{ $grade->name }}" placeholder="班级名称" name="grade[name]">
-                        </div>
-                        <div class="form-group">
-                            <label for="grade-desc-input">简介</label>
-                            <textarea class="form-control" name="grade[description]" id="grade-desc-input" cols="30" rows="10" placeholder="班级简介">{{ $grade->description }}</textarea>
-                        </div>
-                        <?php
-                        Button::Print(['id'=>'btn-save-grade','text'=>trans('general.submit')], Button::TYPE_PRIMARY);
-                        ?>&nbsp;
-                        <?php
-                        Anchor::Print(['text'=>trans('general.return'),'href'=>url()->previous(),'class'=>'pull-right link-return'], Button::TYPE_SUCCESS,'arrow-circle-o-right')
-                        ?>
+                        @include('school_manager.grade._form')
+
                     </form>
                 </div>
             </div>
