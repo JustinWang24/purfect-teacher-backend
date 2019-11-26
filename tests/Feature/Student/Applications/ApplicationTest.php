5a182d685e163335c85b574c11add1ec7cacc032
liuyang <15210631164@163.com>
2019-11-26 10:27:53 +0800

    申请管理模块

--- tests/Feature/Student/Applications/ApplicationTest.php
+++ tests/Feature/Student/Applications/ApplicationTest.php
@@ -13,6 +13,12 @@ class ApplicationTest extends BasicPageTestCase
         $data = [
             'application_type_id' => 1,
             'reason'              => $this->randomCreateChinese(50),
+            'census'              => 1,
+            'family_population'   => 4,
+            'general_income'      => 10000,
+            'per_capita_income'   => 2000,
+            'income_source'       => '打工',
+            'media_id'            => [1],
         ];
 
         return ['application'=>$data];
@@ -30,7 +36,7 @@ class ApplicationTest extends BasicPageTestCase
         $response = $this->post(route('api.students.applications-create'),
             $data,$header);
         $result = json_decode($response->content(),true);
-
+        dd($result);
         $this->assertArrayHasKey('code', $result);
         $this->assertEquals(JsonBuilder::CODE_SUCCESS,
             $result['code']);
