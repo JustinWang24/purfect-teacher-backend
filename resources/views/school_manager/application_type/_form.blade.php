4a661d4604dc94ba617e500be1e35dc9edf70a95
wangyue <justinwang24@yahoo.com.au>
2019-11-26 11:57:21 +0800

    Refine application type form

--- resources/views/school_manager/application_type/_form.blade.php
+++ resources/views/school_manager/application_type/_form.blade.php
@@ -2,8 +2,8 @@
 @csrf
 <div class="col-3">
     <div class="form-group">
-        <label for="major-name-input">设备申请类型</label>
-        <input required type="text" class="form-control" id="type-name-input" value="{{ $type->name ?? old('name') }}" placeholder="设备申请类型" name="type[name]">
+        <label for="type-name-input">申请类型名称</label>
+        <input required type="text" class="form-control" id="type-name-input" value="{{ $type->name ?? old('name') }}" placeholder="必填: 申请类型名称" name="type[name]">
     </div>
 </div>
 
