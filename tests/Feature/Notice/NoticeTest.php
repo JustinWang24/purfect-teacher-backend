a9efa04229e239043f10c82c5ebc7d19da00b06b
wenhao <840346763@qq.com>
2019-11-27 09:03:06 +0800

    通知公告

---
+++ tests/Feature/Notice/NoticeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Tests\Feature\Notice;
+
+use Tests\Feature\BasicPageTestCase;
+
+class NoticeTest extends BasicPageTestCase
+{
+
+    /**
+     * 测试正常添加通知公告
+     */
+    public function testAddNotice()
+    {
+
+
+    }
+
+
+}
