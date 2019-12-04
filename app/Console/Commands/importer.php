ffa0f613726adfafb82ded7f401ccca15cb7e8c6
zhucg <28435993@qq.com>
2019-12-04 18:17:12 +0800

    学校的excel用户资料导入功能
    import_log是日志表
    因为浏览器和web服务器有运行时间限制，所以做成了artisan命令方式执行，命令名为php artisan inporter configId managerId
    因为student_profile表中很多字段是必填的，但是excel文件中没有，所以没有做此表的导入

---
+++ app/Console/Commands/importer.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\BusinessLogic\ImportExcel\Factory;
+use App\Dao\Importer\ImporterDao;
+use App\Dao\Users\UserDao;
+use Illuminate\Console\Command;
+
+class importer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'importer {configId} {userId}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '导入用户资料';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $optUserId = $this->argument('userId');
+        $id = $this->argument('configId');
+        $dao = new ImporterDao();
+        $taskObj = $dao->getTaskById($id);
+        $taskConfig = json_decode($taskObj->config, true);
+        $taskConfig['importerName'] = 'App\BusinessLogic\ImportExcel\Impl\\' . $taskConfig['importerName'] . 'Importer';
+        $taskConfig['file_path'] = $taskObj->file_path;
+        $taskConfig['task_id'] = $id;
+        $taskConfig['manager_id'] = $optUserId;
+        $obj = Factory::createAdapter($taskConfig);
+        $obj->handle();
+    }
+
+}
