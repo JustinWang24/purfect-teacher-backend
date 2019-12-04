64bc91ad8cd9d91c1c7011a033c3d005a254f9b1
liuyang <15210631164@163.com>
2019-12-04 10:02:37 +0800

    修改bug

--- app/Http/Requests/Notice/NoticeRequest.php
+++ app/Http/Requests/Notice/NoticeRequest.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Requests\Notice;
 
+use App\Models\Notices\Notice;
 use App\Http\Requests\MyStandardRequest;
 
 class NoticeRequest extends MyStandardRequest
 {
 
     public function getType() {
-        return $this->get('type');
+        return $this->get('type', Notice::TYPE_NOTIFY);
     }
 
     public function getNoticeId() {
-        return $this->get('notice_id');
+        return $this->get('notice_id',null);
     }
 }
