9c8320829bd8dca89faa3a0ae75722e987f872da
wangyue <justinwang24@yahoo.com.au>
2019-12-05 20:30:39 +0800

    修复 migration 的 bug

--- database/migrations/2019_12_05_140031_update_student_profiles_table111.php
+++ database/migrations/2019_12_05_140031_update_student_profiles_table111.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateStudentProfilesTable extends Migration
+class UpdateStudentProfilesTable111 extends Migration
 {
     /**
      * Run the migrations.
@@ -26,6 +26,9 @@ class UpdateStudentProfilesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('student_profiles', function (Blueprint $table) {
+            $table->dropColumn('account_money');
+            $table->dropColumn('red_envelope');
+        });
     }
 }
