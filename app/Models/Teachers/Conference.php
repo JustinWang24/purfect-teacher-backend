f5ea21da41094af7d0804d3df4bd6483cb752f09
liuyang <15210631164@163.com>
2019-12-11 16:28:04 +0800

    app会议管理接口

--- app/Models/Teachers/Conference.php
+++ app/Models/Teachers/Conference.php
@@ -8,21 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Conference extends Model
 {
 
-    const STATUS_UNCHECK = 0;    //未审核
-    const STATUS_CHECK   = 1;    //审核
-    const STATUS_REFUSE  = 2;    //拒绝
+    const STATUS_UNCHECK  = 0;    //未审核
+    const STATUS_CHECK    = 1;    //审核
+    const STATUS_REFUSE   = 2;    //拒绝
+    const STATUS_WAITING  = 3;    //等待开始
+    const STATUS_UNDERWAY = 4;    //进行中
+    const STATUS_FINISHED = 5;    //已结束
 
     const STATUS_UNCHECK_TEXT = '未审核';
     const STATUS_CHECK_TEXT   = '已审核';
     const STATUS_REFUSE_TEXT  = '拒绝';
 
     protected  $fillable=[
-        'title','school_id','user_id','room_id','sign_out','date','to','from','video','remark',
+        'title','school_id','user_id','room_id','sign_out','to','from','video','remark',
     ];
-
 
     protected $hidden=['updated_at'];
 
+
+    public $room_field = ['name'];
+
+    public $user_field = ['id','uuid','name','mobile'];
 
     public function school()
     {
@@ -36,8 +42,8 @@ class Conference extends Model
      */
     public function user()
     {
-        $field = ['id','uuid','name','mobile'];
-        return $this->belongsTo(User::class)->select($field);
+
+        return $this->belongsTo(User::class)->select($this->user_field);
     }
 
     /**
@@ -47,5 +53,18 @@ class Conference extends Model
     public function participants()
     {
         return $this->hasMany(ConferencesUser::class, 'conference_id', 'id');
+    }
+
+
+    public function room() {
+
+        return $this->belongsTo(User::class)->select($this->room_field);
+    }
+
+
+    public function medias() {
+
+        return $this->hasMany(ConferencesMedia::class);
+
     }
 }
