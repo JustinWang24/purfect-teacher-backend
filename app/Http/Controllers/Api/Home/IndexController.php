fd28c683e00f85d1f4879f5fa55df20c738205a7
wangyue <justinwang24@yahoo.com.au>
2019-12-12 00:14:21 +0800

    校历接口完成

--- app/Http/Controllers/Api/Home/IndexController.php
+++ app/Http/Controllers/Api/Home/IndexController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Api\Home;
 
 use App\Dao\Banners\BannerDao;
+use App\Dao\Calendar\CalendarDao;
+use App\Dao\Schools\SchoolDao;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Banner\BannerRequest;
 use App\Http\Requests\Home\HomeRequest;
 use App\Dao\Schools\NewsDao;
+use App\Http\Requests\MyStandardRequest;
 use App\Utils\JsonBuilder;
+use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
@@ -50,5 +54,37 @@ class IndexController extends Controller
         $dao = new BannerDao;
         $data = $dao->getBannerBySchoolIdAndPosit($request->user()->getSchoolId(), $posit, $publicOnly);
         return JsonBuilder::Success($data);
+    }
+
+    /**
+     * 获取校历的接口
+     * 可以提交学校的 id 或者名称进行查询. 如果未提供, 则以当前登陆用户的信息为准, 获取该用户所在学校的校历信息
+     * 根据当前登陆用户, 当前的调用时间, 获取当前学期校历的接口
+     *
+     * @param MyStandardRequest $request
+     * @return string
+     */
+    public function calendar(MyStandardRequest $request){
+        $schoolIdOrName = $request->get('school', null);
+        $dao = new SchoolDao();
+        if($schoolIdOrName){
+            $school = $dao->getSchoolById($schoolIdOrName);
+            if(!$school){
+                $school = $dao->getSchoolByName($schoolIdOrName);
+            }
+        }
+        else{
+            $school = $dao->getSchoolById($request->user()->getSchoolId());
+        }
+
+        if(!$school){
+            return JsonBuilder::Error('找不到学校的信息');
+        }
+        else{
+            $dao = new CalendarDao();
+            return JsonBuilder::Success([
+                'calendar'=>$dao->getCalendar($school->configuration)
+            ]);
+        }
     }
 }
