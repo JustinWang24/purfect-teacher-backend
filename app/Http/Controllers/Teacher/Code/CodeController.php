44d7b90db4e0fc033d0cbab11b94bfc7d77ba4d2
liuyang <944491509@qq.com>
2019-12-25 13:41:50 +0800

    创建二维码使用记录

--- app/Http/Controllers/Teacher/Code/CodeController.php
+++ app/Http/Controllers/Teacher/Code/CodeController.php
@@ -7,7 +7,6 @@ namespace App\Http\Controllers\Teacher\Code;
 use App\Dao\Users\UserCodeRecordDao;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MyStandardRequest;
-use phpDocumentor\Reflection\DocBlock\Description;
 
 class CodeController extends Controller
 {
