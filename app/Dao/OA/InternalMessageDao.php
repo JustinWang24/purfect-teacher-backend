7b119a6a717b2771705e4bf2e8d0debe536c3e96
wenhao <840346763@qq.com>
2020-01-13 20:05:58 +0800

    内部信临时提交

--- app/Dao/OA/InternalMessageDao.php
+++ app/Dao/OA/InternalMessageDao.php
@@ -1,10 +1,53 @@
 <?php
 
-
 namespace App\Dao\OA;
 
+use App\Models\OA\InternalMessage;
+use App\Models\OA\InternalMessageFile;
+use Illuminate\Support\Facades\DB;
 
 class InternalMessageDao
 {
+
+    public function create($data, $files)
+    {
+        DB::beginTransaction();
+        try{
+            if ($data['is_relay'] == 1) {
+
+            }
+
+            $message = InternalMessage::create($data);
+
+            #处理收件人数据
+            $collId = explode(',', $data['collect_user_id']);
+            $collData = [];
+            foreach ($collId as $key => $value) {
+                $collData[$key]['user_id']           = $data['user_id'];
+                $collData[$key]['collect_user_id']   = $value;
+                $collData[$key]['collect_user_name'] = $data['collect_user_name'];
+                $collData[$key]['title']             = $data['title'];
+                $collData[$key]['content']           = $data['content'];
+                $collData[$key]['type']              = 1;
+                $collData[$key]['is_relay']          = $data['is_relay'];
+                InternalMessage::create($collData);
+            }
+
+            if ($files) {
+                foreach ($files as $key => $val) {
+                    $val['message_id'] = $message->id;
+                    InternalMessageFile::create($val);
+                }
+            }
+            DB::commit();
+            $result = true;
+        }catch (\Exception $e) {
+            dd($e);
+            DB::rollBack();
+            $result = false;
+        }
+
+        return $result;
+    }
 
 }
--- app/Models/OA/InternalMessage.php
+++ app/Models/OA/InternalMessage.php
@@ -1,10 +1,37 @@
 <?php
 
-
 namespace App\Models\OA;
 
+use Illuminate\Database\Eloquent\Model;
 
-class InternalMessage
+class InternalMessage extends Model
 {
+    protected $table = 'oa_internal_messages';
+
+    protected $fillable = ['user_id', 'collect_user_id', 'collect_user_name',
+                           'title', 'content', 'type', 'is_relay', 'message_id',
+                           'status'
+    ];
+
+    const DRIVER_LOCAL      = 1;
+    const DRIVER_ALI_YUN    = 2;
+    const DRIVER_QI_NIU     = 3;
+
+    const DEFAULT_UPLOAD_PATH_PREFIX = 'public/internal';   // 存放用户文件路径
+    const DEFAULT_URL_PATH_PREFIX = '/storage/internal';     // 对外的
+
+    /**
+     * 转换上传路径到 url 路径
+     * @param $uploadPath
+     * @return string
+     */
+    public static function internalMessageUploadPathToUrl($uploadPath)
+    {
+        // 本地图片服务
+        if(env('NETWORK_DISK_DRIVER', self::DRIVER_LOCAL) === self::DRIVER_LOCAL) {
+            return str_replace(self::DEFAULT_UPLOAD_PATH_PREFIX, self::DEFAULT_URL_PATH_PREFIX, $uploadPath);
+        }
+        return '';
+    }
 
 }
