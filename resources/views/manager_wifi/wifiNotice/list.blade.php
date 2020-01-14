b90f4f81778a5e95d795726429f1106d8fec6cfd
Script <zk_admin@163.com>
2020-01-14 11:24:31 +0800

    wifi 大小写问题

---
+++ resources/views/manager_wifi/wifiNotice/list.blade.php
@@ -0,0 +1,56 @@
+<?php
+use App\Utils\UI\Anchor;
+use App\Utils\UI\Button;
+use App\User;
+?>
+@extends('layouts.app')
+@section('content')
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-xl-12">
+            <div class="card-box">
+                <div class="card-head">
+                    <!--header>title1- title2</header-->
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="row table-padding">
+                            <div class="col-12">
+                                <a href="{{ route('manager_wifi.wifiNotice.add') }}" class="btn btn-primary pull-right" id="btn-create-room-from-building">
+                                    添加 <i class="fa fa-plus"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered table-hover table-checkable order-column valign-middle text-center">
+                                <thead>
+                                <tr>
+                                    <th>排序</th>
+									<th>标题</th>
+									<th>添加时间</th>
+									<th>修改时间</th>
+									<th>操作</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($dataList as $key=>$val)
+                                    <tr>
+                                        <td>{{$val['sort']}}</td>
+										<td>{{$val['notice_title']}}</td>
+										<td>{{$val['created_at']}}</td>
+										<td>{{$val['updated_at']}}</td>
+                                        <td class="text-center">
+                                            {{ Anchor::Print(['text'=>'编辑','class'=>'btn-info','href'=>route('manager_wifi.wifiNotice.edit',['noticeid'=>$val->noticeid])], Button::TYPE_DEFAULT,'edit') }}
+                                            {{ Anchor::Print(['text'=>'删除','class'=>'btn-danger','href'=>route('manager_wifi.wifiNotice.delete',['noticeid'=>$val->noticeid])], Button::TYPE_DEFAULT,'delete') }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        {!! $dataList->fragment('feed')->render() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
