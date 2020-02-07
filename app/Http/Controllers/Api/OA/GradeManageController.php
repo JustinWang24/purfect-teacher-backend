199307b70640af35d526dc7c7b02d4a25e900a35
wenhao <840346763@qq.com>
2020-02-07 13:39:15 +0800

    i

--- app/Http/Controllers/Api/OA/GradeManageController.php
+++ app/Http/Controllers/Api/OA/GradeManageController.php
@@ -184,10 +184,13 @@ class GradeManageController extends Controller
 
         $dao = new StudentProfileDao;
         $gradeManagerDao = new GradeManagerDao;
+        $userDao = new UserDao;
+        $userResult = $userDao->updateUser($studentId, null,null,null,$data['email']);
         $gradeResult = $gradeManagerDao->updateGradeManger($monitor['grade_id'], $monitor);
         $studentResult =  $dao->updateStudentProfile($studentId, $data);
         $groupResult = $gradeManagerDao->updateGradeManger($group['grade_id'], $group);
-        if ($gradeResult || $studentResult || $groupResult) {
+
+        if ($gradeResult || $studentResult || $groupResult || $userResult) {
             return JsonBuilder::Success('修改成功');
         } else {
             return JsonBuilder::Error('修改失败');
