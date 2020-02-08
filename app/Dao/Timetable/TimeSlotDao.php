b8f4fbaf3a32d7640609d3a475ef89c7cf005058
文豪 <840346763@qq.com>
2020-02-08 18:09:35 +0800

    测试数据C

--- app/Dao/Timetable/TimeSlotDao.php
+++ app/Dao/Timetable/TimeSlotDao.php
@@ -108,6 +108,7 @@ class TimeSlotDao
      */
     protected function isCurrent($timeSlot){
         $time = now(GradeAndYearUtil::TIMEZONE_CN)->format('H:i:s');
+         // todo :: 测试数据
         $time = Carbon::parse('2020-01-08 14:40:00')->format('H:i:s');
         return $timeSlot->from <= $time && $time < $timeSlot->to;
     }
@@ -170,6 +171,7 @@ class TimeSlotDao
         if(!$date){
             $date = Carbon::now();
         }
+        // todo :: 测试数据
         $date = Carbon::parse('2020-01-08 14:40:00');
         /**
          * @var School $school
