5253458f19f267dd9347113da5fe55e74bd964c7
liuyang <470126642@qq.com>
2020-02-22 21:59:07 +0800

    办公页

--- resources/views/teacher/ly/oa/index.blade.php
+++ resources/views/teacher/ly/oa/index.blade.php
@@ -3,30 +3,10 @@
 <div class="row" id="teacher-oa-index-app">
     <div class="col-sm-12 col-md-12 col-xl-12">
         <div class="teacher_oa_card">
-            <div class="teacher_oa_card_each">
-                <span class="teacher_oa_card_img"></span>
-                <p>通知公告</p>
-            </div>
-            <div class="teacher_oa_card_each">
-                <span class="teacher_oa_card_img"></span>
-                <p>日志</p>
-            </div>
-            <div class="teacher_oa_card_each">
-                <span class="teacher_oa_card_img"></span>
-                <p>内部信</p>
-            </div>
-            <div class="teacher_oa_card_each">
-                <span class="teacher_oa_card_img"></span>
-                <p>会议</p>
-            </div>
-            <div class="teacher_oa_card_each">
-                <span class="teacher_oa_card_img"></span>
-                <p>公文</p>
-            </div>
-            <div class="teacher_oa_card_each">
-                <span class="teacher_oa_card_img"></span>
-                <p>任务</p>
-            </div>
+            <dl class="teacher_oa_card_body" v-for="item in iconList" :key="item.name">
+                <dt><img :src="item.icon" alt=""></dt>
+                <dd>@{{ item.name }}</dd>
+            </dl>
         </div>
         <p class="teacher_oa_approval">我的审批</p>
         <div class="teacher_oa_approval_content">
@@ -42,7 +22,7 @@
                         <el-button slot="append">搜索</el-button>
                     </el-input>
                     <div>
-                        
+
                     </div>
                 </div>
             </div>
