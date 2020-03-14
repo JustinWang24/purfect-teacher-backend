1ac4dfdb2b1f11c1765fca0fdbafd02fbc8c6e76
21dexiaoyu <21dexiaoyu@163.com>
2020-03-14 20:55:30 +0800

    工作流程 bug

--- resources/views/h5_apps/pipeline/flow_view_history.blade.php
+++ resources/views/h5_apps/pipeline/flow_view_history.blade.php
@@ -106,17 +106,15 @@
             </h3>
             <div class="block" style="padding: 0 15px;">
 
-                <el-timeline>
-                    <el-timeline-item key="">
-                          <el-timeline-item  :timestamp="{{ substr($startAction->created_at, 5, 11) }}">
-                            <img src="{{ $startUser->profile->avatar }}" alt="" style="width: 40px; height: 40px;border-radius: 50%;vertical-align: middle;">
-                            {{ $startUser->name }}
-                            <span style="text-align: right;"> 发起审批 </span>
-                          </el-timeline-item>
-                    </el-timeline-item>
-                </el-timeline>
 
                 <el-timeline>
+                  <el-timeline-item key="">
+                    <el-timeline-item  :timestamp="{{ substr($startAction->created_at, 5, 11) }}">
+                      <img src="{{ $startUser->profile->avatar }}" alt="" style="width: 40px; height: 40px;border-radius: 50%;vertical-align: middle;">
+                      {{ $startUser->name }}
+                      <span style="text-align: right;"> 发起审批 </span>
+                    </el-timeline-item>
+                  </el-timeline-item>
                     @foreach($handlers as $key => $handler)
                     <!-- <el-timeline-item key="{{ $key }}" icon="审批状态"  :timestamp="时间戳2018-04-12 20:46">-->
                     <el-timeline-item key="{{ $key }}">
