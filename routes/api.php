43d0055eb68457ce9351cb084acff7f95f8bed53
wenhao <840346763@qq.com>
2020-03-16 12:09:24 +0800

    路由

--- routes/api.php
+++ routes/api.php
@@ -537,7 +537,7 @@ Route::prefix('attendance')->middleware('auth:api')->group(function () {
         ->name('api.get.day.course');
 
     // 获取教师签到统计
-    Route::post('/get-teacher-statistics', 'Api\AttendanceSchedule\add-messageadd-message@getTeacherCourseStatistics')
+    Route::post('/get-teacher-statistics', 'Api\AttendanceSchedule\AttendanceController@getTeacherCourseStatistics')
         ->name('api.get.teacher.statistics');
 
     // 获取教师签到统计详情
