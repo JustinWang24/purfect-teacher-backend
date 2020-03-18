92a4bbd491f971674ecaa36ef243e4daaf4ec3a2
liuyang <470126642@qq.com>
2020-03-18 22:53:52 +0800

    1045 1014 992 991 1009 1047bug

--- resources/views/h5_apps/pipeline/flow_copy_to_me.blade.php
+++ resources/views/h5_apps/pipeline/flow_copy_to_me.blade.php
@@ -7,21 +7,24 @@
             <i class="el-icon-loading"></i>&nbsp;数据加载中 ...
         </p>
     </div>
-    <div class="main p-15">
-        @foreach($list as $item)
-        <div class="pipeline-user-flow-box" @click="viewMyApplication({{ $item->userFlow }})">
+    <div class="main p-15" v-if="copyList.length > 0">
+        <el-input placeholder="搜索标题、发起人关键字" v-model="keyword" style="margin-bottom: 10px;" @input="loadFlowsCopyByMe"></el-input>
+        <div class="pipeline-user-flow-box" v-for="(userFlow, idx) in copyList" :key="idx" @click="viewMyApplication(userFlow)">
             <el-card shadow="hover" class="pb-3">
                 <div style="display: flex;align-items: center;">
-                    <img src="@if(isset($item->user->profile->avatar)){{ $item->user->profile->avatar }}@endif" style="border-radius: 50%;width: 40px;height: 40px;">
+                    <img :src="userFlow.avatar" style="border-radius: 50%;width: 40px;height: 40px;">
                     <h3 style="margin-left: 20px;flex: 4;">
-                        <p style="line-height: 0;font-weight:400;color:#333333;">{{ $item->user->name }}的{{ $item->flow->name }}申请</p>
-                        <p style="font-size: 13px;color: #ABABAB;margin: 0">类型：{{ $item->flow->name }}</p>
-                        <time style="font-size: 13px;color: #ABABAB;">{{ substr($item->created_at, 0, 16) }}</time>
+                        <p style="line-height: 0;font-weight:400;color:#333333;">@{{ userFlow.user_name }}的@{{ userFlow.flow.name }}申请</p>
+                        <p style="font-size: 13px;color: #ABABAB;margin: 0">类型：@{{ userFlow.flow.name }}</p>
+                        <time style="font-size: 13px;color: #ABABAB;">@{{ userFlow.created_at.substring(0, 16) }}</time>
                     </h3>
                 </div>
             </el-card>
         </div>
-        @endforeach
+    </div>
+    <div v-else style="display: flex;flex-direction: column;align-items: center;background-color: #fff;margin-top: 150px;">
+        <img src="{{asset('assets/img/pipeline/nothing@2x.png')}}" alt="" style="width: 290px;height: 220px;">
+        <p style="color: #6F7275;text-align: center;font-family:PingFangSC-Regular,PingFang SC;">暂无数据哦~</p>
     </div>
 </div>
 @endsection
