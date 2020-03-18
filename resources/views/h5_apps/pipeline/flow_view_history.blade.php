e3a77c551f4c02f71271df6a45efd5e089a230e8
liuyang <470126642@qq.com>
2020-03-19 00:01:33 +0800

    审批意见样式修改

--- resources/views/h5_apps/pipeline/flow_view_history.blade.php
+++ resources/views/h5_apps/pipeline/flow_view_history.blade.php
@@ -152,6 +152,10 @@
             <el-button type="primary" style="width: 40%;border-radius: 50px;margin-bottom: 20px;" @click="dialogVisible = true">审批</el-button>
         </div>
         @endif
+
+
+
+        
         <el-dialog title="审批" :visible.sync="dialogVisible" width="90%" center>
             <el-input type="textarea" :rows="6" placeholder="请输入审批意见" v-model="textarea" maxlength="100"></el-input>
             <span style="position: relative;top: -18px;left: 85%;">@{{textarea.length}}/100</span>
