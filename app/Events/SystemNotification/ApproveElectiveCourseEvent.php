109003cd7363c07b31b66b3ca5c0ae73779e9bd7
21dexiaoyu <21dexiaoyu@163.com>
2020-03-19 15:40:45 +0800

    选修课消息通知

---
+++ app/Events/SystemNotification/ApproveElectiveCourseEvent.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Events\SystemNotification;
+
+use App\Events\CanSendSystemNotification;
+use App\Models\Course;
+use App\Models\Misc\SystemNotification;
+use App\Models\RecruitStudent\RegistrationInformatics;
+use App\Models\Wifi\Backstage\WifiIssues;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class ApproveElectiveCourseEvent implements CanSendSystemNotification
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    private  $user;
+    private  $course;
+    private  $result;
+
+    /**
+     * ApproveElectiveCourseEvent constructor.
+     * @param User $user
+     * @param $courseId
+     */
+    public function __construct(User $user, Course $course, $result)
+    {
+        $this->user = $user;
+        $this->$course = $course;
+        $this->result = $result;
+    }
+
+    /**
+     * 必须可以拿到接收学校
+     * @return int
+     */
+    public function getSchoolId(): int
+    {
+        return $this->user->school_id;
+    }
+
+    /**
+     * 可以拿到接收者
+     * @return int
+     */
+    public function getTo(): int
+    {
+        return $this->user->id;
+    }
+
+    /**
+     * 必须可以拿到发送者
+     * @return int
+     */
+    public function getSender(): int
+    {
+        return SystemNotification::FROM_SYSTEM;//系统
+    }
+
+    /**
+     * 必须可以拿到消息类别
+     * @return int
+     */
+    public function getType(): int
+    {
+        return SystemNotification::TYPE_NONE;
+    }
+
+    /**
+     * 必须可以拿到消息级别
+     * @return int
+     */
+    public function getPriority(): int
+    {
+        return SystemNotification::PRIORITY_LOW;
+    }
+    /**
+     * 必须可以拿到发送标题
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return '选修课程(' . $this->course->name . ')' . $this->result ? '已开课！': '已取消！';
+    }
+
+    /**
+     * 必须可以拿到发送内容
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return '选修课程(' . $this->course->name . ')' . $this->result ? '已开课！': '已取消！';
+    }
+
+    /**
+     * 必须可以拿到消息分类
+     * @return int
+     */
+    public function getCategory(): int
+    {
+        return SystemNotification::STUDENT_CATEGORY_COURSE;
+    }
+
+    /**
+     * 必须可以拿到前端所需key
+     * @return string
+     */
+    public function getAppExtra(): string
+    {
+        $extra = [
+            'type' => 'course-elective-details',
+            'param1' => $this->course->id,
+            'param2' => ''
+        ];
+        return json_encode($extra);
+    }
+
+    /**
+     * 必须可以拿到下一步
+     * @return string
+     */
+    public function getNextMove(): string
+    {
+        return '';
+    }
+
+    /**
+     * 必须可以拿到组织id
+     * @return array
+     */
+    public function getOrganizationIdArray(): array
+    {
+        return [];//单人消息 无组织可见范围
+    }
+
+}
