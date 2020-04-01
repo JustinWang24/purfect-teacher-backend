8673676e8ed97c688bb57374a49cf650ccc67cbd
liuyang <944491509@qq.com>
2020-04-01 17:34:08 +0800

    接受参数

--- app/Http/Controllers/Api/AttendanceSchedule/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceSchedule/AttendanceController.php
@@ -268,7 +268,7 @@ class AttendanceController extends Controller
             return JsonBuilder::Error('未找到当前学生要上的的课程');
         }
 
-        $timetableItem = $timetableItemDao->getTimeTableItemById($code['item_id']);
+        $timetableItem = $timetableItemDao->getTimeTableItemById($code['itme_id']);
 
         $data = [
             'timetable_id' => $timetableItem->id,
